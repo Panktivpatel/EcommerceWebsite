719eec5abe5fec7d8da99dd67774da6b1fa21f55
Pankti Patel <pn853367@dal.ca>
2023-09-06 14:10:18 -0300

    Updates

--- frontend/src/pages/Constant/NavBar.js
+++ frontend/src/pages/Constant/NavBar.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-
 import "../../css/Constant/NavBar.css";
 
 const NavBar = () => {
@@ -11,27 +10,43 @@ const NavBar = () => {
     };      
 
     return (
-        <div className="navbar">
-            <div className="navbar-brand">
-                <button className="navbar-toggle" onClick={toggleMenu}>
-                <img className="navbar-toggle-icon" src=""/>
-                </button>
-            </div>
-            <div>
-            <ul className={`navbar-menu ${isMenuOpen ? 'open' : ''}`}>
-                    <li className="navbar-item">Home</li>
-                    <li className="navbar-item"> Our Products</li>
-                    <li className="navbar-item">New Arrivals</li>
-                    <li className="navbar-item">Deals</li>
-                    <li className="navbar-item">Contact Us</li>
-                    <li className="navbar-item">
-                    <button className="navbar-toggle" onClick={toggleMenu}>
-                        <span className="navbar-toggle-icon">Login</span>
+        <div>
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">Navbar</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
                     </button>
-                    </li>
-                    <li className="navbar-item">Cart</li>
-                </ul>
-            </div>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><hr class="dropdown-divider"/></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link disabled">Disabled</a>
+                        </li>
+                    </ul>
+                    <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                    </div>
+                </div>
+            </nav>
         </div>
     );
 };
